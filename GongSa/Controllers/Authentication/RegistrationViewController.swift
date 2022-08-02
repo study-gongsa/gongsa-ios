4f8dc3a45c4a7d9dd018ab8f811935195b9833d2
tcha <hataeck@gmail.com>
2022-08-02 11:05:25 +0900

    refactor: 버전 처리

--- GongSa/Controllers/Authentication/RegistrationViewController.swift
+++ GongSa/Controllers/Authentication/RegistrationViewController.swift
@@ -24,11 +24,19 @@ class RegistrationViewController: UIViewController {
         // TODO: 유저 register 코드
         
         // MainTabViewController 가져와서 인증, UI 그리고 dismiss
-        let scenes = UIApplication.shared.connectedScenes
-        let windowScene = scenes.first as? UIWindowScene
-        guard let window = windowScene?.windows.first(where: { $0.isKeyWindow }) else { return }
-        guard let tab = window.rootViewController as? MainTabViewController else { return }
+        let window: UIWindow
+        if #available(iOS 13.0, *) {
+            let scenes = UIApplication.shared.connectedScenes
+            let windowScene = scenes.first as? UIWindowScene
+            guard let firstWindow = windowScene?.windows.first(where: { $0.isKeyWindow }) else { return }
+            window = firstWindow
+            
+        } else {
+            guard let firstWindow = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
+            window = firstWindow
+        }
         
+        guard let tab = window.rootViewController as? MainTabViewController else { return }
         tab.authenticateUserAndConfigureUI()
 
         self.dismiss(animated: true, completion: nil)
