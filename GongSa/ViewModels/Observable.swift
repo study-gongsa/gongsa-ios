3406ae1d94565b9cd20c034d9f47210da03fab4e
Ian <chr4908@gmail.com>
2022-08-19 00:59:40 +0900

    Create Observable.swift
    feat: Observable.swift 생성
    
    Observable.swift 생성

---
+++ GongSa/ViewModels/Observable.swift
@@ -0,0 +1,29 @@
+//
+//  Observable.swift
+//  GongSa
+//
+//  Created by Chaerin Han on 2022/08/19.
+//
+
+import Foundation
+
+class Observable<T> {
+    typealias Listener = (T) -> Void
+    private var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+
+    init(_ value: T) {
+        self.value = value
+    }
+
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
