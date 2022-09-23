5b5da7c58de76dc5a04602be8b9c6e87c36c9d68
Ian <chr4908@gmail.com>
2022-09-23 16:55:33 +0900

    feat: FindPasswordLoadingViewController 생성

---
+++ GongSa/Controllers/Authentication/ForgotPassword/FindPasswordLoadingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FindPasswordLoadingViewController.swift
+//  GongSa
+//
+//  Created by Chaerin Han on 2022/09/23.
+//
+
+import Foundation
+import UIKit
+
+
+class FindPasswordLoadingView: UIViewController {
+    
+}
