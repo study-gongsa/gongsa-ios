dc4567b08f36da84fa746f70b392f0e9b1dcac96
Ian <chr4908@gmail.com>
2022-10-29 19:54:57 +0900

    fix: add }

--- GongSa/Controllers/Authentication/ForgotPassword/Password-Network/PasswordService.swift
+++ GongSa/Controllers/Authentication/ForgotPassword/Password-Network/PasswordService.swift
@@ -20,7 +20,7 @@ struct PasswordService{
     }
     
     func findPassword(email : String,
-               completion : @escaping (NetworkResult<Any>) -> Void)
+                      completion : @escaping (NetworkResult<Any>) -> Void)
     {
         let header : HTTPHeaders = ["Content-Type": "application/json"]
         let URL = "http://3.36.170.161:8080/api/user/mail/passwd"
@@ -36,7 +36,7 @@ struct PasswordService{
             dump(dataResponse)
             
             switch dataResponse.result {
-            // 통신 성공
+                // 통신 성공
             case .success:
                 // dataResponse.statusCode 는 Response의 statuscode
                 guard let statusCode = dataResponse.response?.statusCode else {return}
@@ -45,8 +45,8 @@ struct PasswordService{
                 // judgeStatus에 statuscode와 value(결과 데이터) 실어서 보냄
                 let networkResult = self.judgeStatus(by: statusCode, value)
                 completion(networkResult)
-            // 통신 실패의 경우 completion에 pathErr값을 담아서 뷰컨으로 날려주기
-            // 타임아웃 / 통신 불가능의 상태로 통신 자체에 실패한 경우
+                // 통신 실패의 경우 completion에 pathErr값을 담아서 뷰컨으로 날려주기
+                // 타임아웃 / 통신 불가능의 상태로 통신 자체에 실패한 경우
             case .failure: completion(.pathErr)
                 guard (dataResponse.response?.statusCode) != nil else {return}
             }
@@ -70,3 +70,4 @@ struct PasswordService{
     }
     
     
+}
