fafd0ad3578a6cb675277930b0ddf710c2126a5c
Ian <chr4908@gmail.com>
2022-10-30 02:59:44 +0900

    fix: 로그인 후 Keychain 변경
    Keychain에 토큰 저장 방법 변경

--- GongSa/Controllers/Authentication/LoginViewController.swift
+++ GongSa/Controllers/Authentication/LoginViewController.swift
@@ -346,17 +346,18 @@ class LoginViewController: UIViewController {
         LoginService.shared.login(email: self.emailTxtField.text!, passwd: self.pwTxtField.text!) { result in
             switch result
             {
-            case .success(let data):
+            case.success(let userdata):
+                print("로그인 통신 성공")
                 
-                // 토큰 정보저장
-                if let userData = data as? LoginResponse {
+                if let data = userdata as? LoginDataClass {
                     
-                    let accessToken = userData.data.accessToken
-                    let refreshToken = userData.data.refreshToken
-                    print("accessToken: ",accessToken, "  ***refresh: ", refreshToken)
-                    let tk = TokenUtils()
-                    tk.create(" ", account: "accessToken", value: accessToken)
-                    tk.create(" ", account: "refreshToken", value: refreshToken)
+                    let accessToken = data.accessToken
+                    let refreshToken = data.refreshToken
+                    
+                    // keychain 저장
+                    KeyChain.shared.create(key: "accessToken", token: accessToken)
+                    KeyChain.shared.create(key: "refreshToken", token: refreshToken)
+                    print("Keychain 토큰 저장 성공")
                     
                 }
                 // 로그인 성공 -> 메인화면으로 가기
@@ -380,6 +381,7 @@ class LoginViewController: UIViewController {
         }
         
     }
+    
     func highlightTextField(_ textField: UITextField) {
         textField.resignFirstResponder()
         textField.layer.borderWidth = 1.0
--- GongSa/Views/Login-Network/LoginResponse.swift
+++ GongSa/Views/Login-Network/LoginResponse.swift
@@ -11,43 +11,9 @@ import Foundation
 // MARK: - LoginResponse
 struct LoginResponse: Codable {
     let location, msg: String?
-    let data: DataClass
+    let data: LoginDataClass
 }
-
-// MARK: - DataClass
-struct DataClass: Codable {
+// MARK: - LoginDataClass
+struct LoginDataClass: Codable {
     let accessToken, refreshToken: String
 }
-
-/*
-// MARK: - Encode/decode helpers
-
-class JSONNull: Codable, Hashable {
-
-    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
-        return true
-    }
-
-    public var hashValue: Int {
-        return 0
-    }
-//    func hash(into hasher: inout Hasher) -> Int {
-//        return 0
-//    }
-
-    public init() {}
-
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        if !container.decodeNil() {
-            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
-        }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encodeNil()
-    }
-}
-
-*/
